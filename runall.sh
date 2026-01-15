1de4c500e906f45cdf986783dbb51663cf42dab7
ralph yozzo <ralph@brooklynmarathon.com>
2026-01-15 15:20:19 -0500

    initial load

--- runall.sh
+++ runall.sh
@@ -1,2 +1,3 @@
 #!
-find ./ceaweaver/ ./amp_tweets/ -type f  -name "*.html" -not -name "tweet*"  -print |grep html$  |sort -r |python generate_amp_index.py
+python convert_to_amp.py
+find ./ceaweaver/ ./amp_tweets/ -type f  -name "*.html" -not -name "tweet*"  -print |grep html$  |sort |python generate_amp_index.py
