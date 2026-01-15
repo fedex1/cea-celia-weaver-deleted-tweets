c312ebd6a5d66bac4122f70170128f03b8eb9dbc
ralph yozzo <ralph@brooklynmarathon.com>
2026-01-15 18:18:33 -0500

    initial load

--- generate_amp_index.py
+++ generate_amp_index.py
@@ -11,7 +11,7 @@ AMP_HEADER = f"""<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>{PAGE_TITLE}</title>
-  <link rel="canonical" href="{OUTPUT_FILE}">
+  <link rel="canonical" href="https://history.tidalforce.org/{OUTPUT_FILE}">
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <style amp-boilerplate>body{{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}}@-webkit-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@-moz-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@-ms-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@-o-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}</style><noscript><style amp-boilerplate>body{{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}}</style></noscript>
   <script async src="https://cdn.ampproject.org/v0.js"></script>
