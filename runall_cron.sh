00e3f08641e26c670e1b72f3b1f2c9a7302eb211
ralph yozzo <ralph@brooklynmarathon.com>
2026-05-05 09:57:51 -0400

    ralph-desktop

--- runall_cron.sh
+++ runall_cron.sh
@@ -21,4 +21,4 @@ fi
 AGO=$(date -d "1 day ago" +%Y-%m-%d)
 python firecrawl-test.py "site:x.com micah lasher after:$AGO" |jq -c '.data.web[]' >firecrawl.$DATE.jsonl
 jq -r '"🔗 URL: \(.url)\n📌 Title: \(.title)\n📝 Desc: \(.description)\n🔢 Rank: \(.position)\n---"' <firecrawl.$DATE.jsonl >firecrawl.$DATE.jsonl.txt
-curl --location   --data-binary @firecrawl.$DATE.jsonl.txt   --trace-ascii debug_log.txt   "https://script.google.com/macros/s/AKfycbzPV-to5wu164gyz-c04BKiDG4hwm4E_2eG9viWFeL8Kc-SGZQGFohlZZ9WodgJwXCk/exec" --data "subject=Timmins For Congress latest social search research" --data "to=ralph@brooklynmarathon.com,danny@timminsforcongress.com"
+curl --location   --data-binary @firecrawl.$DATE.jsonl.txt   --trace-ascii debug_log.txt   "https://script.google.com/macros/s/AKfycbzPV-to5wu164gyz-c04BKiDG4hwm4E_2eG9viWFeL8Kc-SGZQGFohlZZ9WodgJwXCk/exec" --data "subject=Timmins For Congress latest social search research" --data "to=ralph@brooklynmarathon.com,patrick@timminsforcongress.com,stimmins13@gmail.com,colin@timminsforcongress.com"
